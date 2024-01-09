71597716df190408636ea6827c8999b076ec31f1
Thomas Basham <Bashamtg@gmail.com>
2024-01-08 17:17:27 -0800

    modulated code for renderMusic function

--- js/musicUI.js
+++ js/musicUI.js
@@ -1,21 +1,19 @@
 "use strict";
-function getMusicData() {
-  $.get("https://itunes.apple.com/search?term=beyonce", function (data) {
-    
-    // need to parse data for the itunes api. 
-    data = JSON.parse(data);
 
-    data.results.forEach((result) => {
-      $("#music-catalogue").append(
-        `<div class="song-div">
-          <p class="pill artist "> ${result.artistName} </p>
-          <p class="pill track"> ${result.trackName} </p>
-        </div>
-        `
-      );
-    });
+function renderMusic(data) {
+  // need to parse data for the itunes api.
+  data = JSON.parse(data);
+  console.log(data);
+
+  data.results.forEach((result) => {
+    $("#music-catalogue").append(
+      `<div class="song-div">
+        <p class="pill artist "> ${result.artistName} </p>
+        <p class="pill track"> ${result.trackName} </p>
+      </div>
+      `
+    );
   });
 }
-
 // Remember to export variables or functions you need
-export default getMusicData;
+export default renderMusic;
