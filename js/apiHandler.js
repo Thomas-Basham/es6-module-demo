74fdba07fbe2fd6c444261b20ff08feaa4f733f2
Thomas Basham <Bashamtg@gmail.com>
2024-01-08 17:16:45 -0800

    split up code for api call

--- js/apiHandler.js
+++ js/apiHandler.js
@@ -1,7 +1,8 @@
+"use strict";
 import renderMusic from "./musicUI.js";
 
 function getMusicData() {
   $.get("https://itunes.apple.com/search?term=beyonce", renderMusic);
 }
 
-export default getMusicData
+export default getMusicData;
